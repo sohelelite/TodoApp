45808bb9fbc3095c8ba039e978f4db9a299e5771
Sohel Shaikh <sohel.shaikh.s@gmail.com>
2015-04-29 16:33:47 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -9,16 +9,12 @@ ionicApp.run(function($ionicPlatform, $rootScope, $timeout) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    //if(window.cordova && window.cordova.plugins.Keyboard) {
-    //  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    //}
-    //if(window.StatusBar) {
-    //  StatusBar.styleDefault();
-    //}
-      console.log('i m in platform ready');
-      console.log("cordova:",$ionicPlatform.isCordova());
-      console.log("paltform:",$ionicPlatform.platform());
-      console.log("version:",$ionicPlatform.version());
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
 
       window.plugin.notification.local.onadd = function (id, state, json) {
           var notification = {
@@ -36,38 +32,35 @@ ionicApp.run(function($ionicPlatform, $rootScope, $timeout) {
 
 ionicApp.controller("ExampleController", function($scope,$ionicPlatform, $cordovaLocalNotification) {
     console.log('i m in controller');
-    $ionicPlatform.ready(function() {
-        alert('Device is Ready');
-    });
 
     $ionicPlatform.ready(function() {
-        console.log('i m ionicplatform ready function');
+        $scope.$on("$cordovaLocalNotification:added", function(id, state, json) {
+            alert("Added a notification");
+            console.log('notification added hurray');
+        });
+
+        $scope.add = function() {
+            var alarmTime = new Date();
+            alarmTime.setMinutes(alarmTime.getMinutes() + 1);
+            $cordovaLocalNotification.add({
+                id: "1234",
+                date: alarmTime,
+                message: "This is a message",
+                title: "This is a title",
+                autoCancel: true,
+                sound: null
+            }).then(function () {
+                console.log("The notification has been set");
+            });
+        };
+
+        $scope.isScheduled = function() {
+            $cordovaLocalNotification.isScheduled("1234").then(function(isScheduled) {
+                alert("Notification 1234 Scheduled: " + isScheduled);
+            });
+        }
     });
 
-    $scope.$on("$cordovaLocalNotification:added", function(id, state, json) {
-        alert("Added a notification");
-        console.log('notification added hurray');
-    });
 
-    $scope.add = function() {
-        var alarmTime = new Date();
-        alarmTime.setMinutes(alarmTime.getMinutes() + 1);
-        $cordovaLocalNotification.add({
-            id: "1234",
-            date: alarmTime,
-            message: "This is a message",
-            title: "This is a title",
-            autoCancel: true,
-            sound: null
-        }).then(function () {
-            console.log("The notification has been set");
-        });
-    };
-
-    $scope.isScheduled = function() {
-        $cordovaLocalNotification.isScheduled("1234").then(function(isScheduled) {
-            alert("Notification 1234 Scheduled: " + isScheduled);
-        });
-    }
 
 });
