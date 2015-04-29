3709c5345c09f0f6995ff8c63bc7021f50e6ff2a
Sohel Shaikh <sohel.shaikh.s@gmail.com>
2015-04-29 17:48:39 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -40,8 +40,33 @@ ionicApp.controller("ExampleController", function($scope,$ionicPlatform, $cordov
     });
 
     document.addEventListener("deviceready", function () {
-        alert('i m device ready');
-    }, false);
+        console.log('im ready device ready');
+        $scope.$on("$cordovaLocalNotification:added", function(id, state, json) {
+            alert("Added a notification");
+            console.log('notification added hurray');
+        });
+
+        $scope.add = function() {
+            var alarmTime = new Date();
+            alarmTime.setMinutes(alarmTime.getMinutes() + 1);
+            $cordovaLocalNotification.add({
+                id: "1234",
+                date: alarmTime,
+                message: "This is a message",
+                title: "This is a title",
+                autoCancel: true,
+                sound: null
+            }).then(function () {
+                console.log("The notification has been set");
+            });
+        };
+
+        $scope.isScheduled = function() {
+            $cordovaLocalNotification.isScheduled("1234").then(function(isScheduled) {
+                alert("Notification 1234 Scheduled: " + isScheduled);
+            });
+        }
+    });
 
     $ionicPlatform.ready(function() {
         $scope.$on("$cordovaLocalNotification:added", function(id, state, json) {
